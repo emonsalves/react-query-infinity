7300799185eff83b1c6b2c372b29d52f50178837
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2023-11-12 04:10:57 -0300

    Add pagination functionality to PokemonList component

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -6,12 +6,18 @@ import { getPokemons } from '../api/querys/getPokemons';
 
 const PokemonList = () => {
     const result = useInfiniteQuery({
+        // se usa el useInfiniteQuery para hacer la paginacion
         getNextPageParam: (lastPage) => lastPage.next,
+        // se usa el getNextPageParam para obtener la url de la siguiente pagina
         queryFn: ({ pageParam }) => getPokemons({ url: pageParam }),
+        // se usa el queryFn para obtener la data de la pagina
         queryKey: ['pokemons'],
+        // se usa el queryKey para que no se repita la peticion
     });
     const { data, status, fetchNextPage, error } = result;
+    // se desestructura el result para obtener la data, el status, la funcion fetchNextPage y el error
     const { ref, inView } = useInView({ threshold: 0 });
+    // se usa el useInView para saber si el ultimo pokemon de la pagina esta en el viewport
 
     useEffect(() => {
         if (inView) {
@@ -31,12 +37,16 @@ const PokemonList = () => {
                     data.pages.map((page) => {
                         return page.results.map((pokemon, index) => {
                             return (
+                                // aca se usa el fragment para que no se repita el key en el div
                                 <Fragment key={pokemon.name}>
+                                    {/* si es el ultimo pokemon de la pagina se le agrega el ref */}
+                                    {/* para que cuando se haga scroll se renderize el siguiente pokemon */}
                                     {index === page.results.length - 1 ? (
                                         <div ref={ref}>
                                             <PokemonCard pokemonName={pokemon.name} />
                                         </div>
                                     ) : (
+                                        // si no es el ultimo pokemon se renderiza normal
                                         <PokemonCard pokemonName={pokemon.name} />
                                     )}
                                 </Fragment>
